245b5639f46412cc228b66224d87244792f924ae
Sione Yerkovich <sioneyerkovich01@gmail.com>
2025-03-12 20:07:31 +1300

    Feature/Fix
    Feature:
    1. Added a home button to the navbar for access to the home page when navigating
    2. Created DailyBreakfast.html and DailyLunch.html (main/templates)
    3. Completed DailyBreakfast styling
    4. Added numerous images for DailyBreakfast (static/Main/images)
    5. Navbar now redirects appropriately to the corresponding page on click
    
    Fix:
    1. Corrected folder location and hierarchy, templates is now the parent of another main folder for django comprehension
    2. Adjusted logo in footer to make it consistent with the navbar
    3. Adjusted social media links to make them cohesive with new logo size
    4. Adjusted spacing in navbar to make it responsive to more devices

--- CookingWebsite/main/urls.py
+++ CookingWebsite/main/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view, name="TCC-home"),
-    path("daily-recipes/", views.daily_recipes_view, name="TCC-daily-recipes"),
+    path("daily-recipes/breakfast/", views.daily_recipes_breakfast_view, name="daily-recipes-breakfast"),
+    path("daily-recipes/lunch/", views.daily_recipes_lunch_view, name="daily-recipes-lunch"),
     path("holidays/", views.holidays_view, name="TCC-holidays"),
 
 ]
--- CookingWebsite/main/views.py
+++ CookingWebsite/main/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(response):
-    return render(response, "home.html")
+    return render(response, "main/home.html")
 
-def daily_recipes_view(response):
-    return HttpResponse("<h1>This is the daily recipes page<h1>")
+def daily_recipes_breakfast_view(response):
+    return render(response, "main/DailyBreakfast.html")
+
+def daily_recipes_lunch_view(response):
+    return render(response, "main/DailyLunch.html")
 
 def holidays_view(response):
     return HttpResponse("<h1>This is the holidays page<h1>")
