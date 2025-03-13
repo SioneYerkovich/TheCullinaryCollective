fba6f163673a22c942b26de31d17b43dda61363b
Sione Yerkovich <sioneyerkovich01@gmail.com>
2025-03-14 10:47:56 +1300

    Feature/Style (main)
    Feature:
    1. Created DailyDinner.html (templates/main) and completed styling that is consistent with sibling files
    2. Created connection for DailyDinner ensuring navbar works correctly
    3. Completed styling for DailyLunch.html (templates/main)
    4. Started work on modal popups for recipe displays when user clicks a recipe in DailyRecipes
    
    Style:
    1. Added styling to all DailyRecipe pages, they now have hover zoom and opaque effects

--- CookingWebsite/main/urls.py
+++ CookingWebsite/main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home_view, name="TCC-home"),
     path("daily-recipes/breakfast/", views.daily_recipes_breakfast_view, name="daily-recipes-breakfast"),
     path("daily-recipes/lunch/", views.daily_recipes_lunch_view, name="daily-recipes-lunch"),
+    path("daily-recipes/dinner/", views.daily_recipes_dinner_view, name="daily-recipes-dinner"),
     path("holidays/", views.holidays_view, name="TCC-holidays"),
 
 ]
--- CookingWebsite/main/views.py
+++ CookingWebsite/main/views.py
@@ -11,5 +11,8 @@ def daily_recipes_breakfast_view(response):
 def daily_recipes_lunch_view(response):
     return render(response, "main/DailyLunch.html")
 
+def daily_recipes_dinner_view(response):
+    return render(response, "main/DailyDinner.html")
+
 def holidays_view(response):
     return HttpResponse("<h1>This is the holidays page<h1>")
