e7d4cf807e1050187414c6707dc02abb3f663727
Sione Yerkovich <sioneyerkovich01@gmail.com>
2025-03-17 12:38:43 +1300

    Feature/Fix/Style/Docs (main)
    Style:
    1. Completed styling for Login.html
    
    Feature:
    1. Added buttons for password recovery and account registration to Login.html
    2. Created and completed Register.html (templates)
    3. Users may now register an account that will be added to the database for login functionality (to be completed)
    
    Fix:
    1. Login.html has now been linked to all existing pages and will redirect accordingly
    2. Updated naming convention in main/urls.py for login.html path. This has been changed from 'login' to 'TCC-login' to remain consistent with similar pages.
    
    Docs:
    1. Updated commentary to code on all pages to follow a maintenance best practices

--- CookingWebsite/main/urls.py
+++ CookingWebsite/main/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path("daily-recipes/breakfast/", views.daily_recipes_breakfast_view, name="daily-recipes-breakfast"),
     path("daily-recipes/lunch/", views.daily_recipes_lunch_view, name="daily-recipes-lunch"),
     path("daily-recipes/dinner/", views.daily_recipes_dinner_view, name="daily-recipes-dinner"),
-    path("Login/", views.login_view, name="login"),
+    path("Login/", views.login_view, name="TCC-login"),
+    path("Login/Register", views.register_view, name="TCC-register"),
     path("holidays/", views.holidays_view, name="TCC-holidays"),
 
 ]
--- CookingWebsite/main/views.py
+++ CookingWebsite/main/views.py
@@ -1,22 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Member
+from .forms import MemberForm
 
 # Create your views here.
+#Function that will return the home.html template and render it to the browser
 def home_view(response):
     return render(response, "main/home.html")
 
+#Function that will return the DailyBreakfast.html template and render it to the browser
 def daily_recipes_breakfast_view(response):
     return render(response, "main/DailyBreakfast.html")
 
+#Function that will return the DailyLunch.html template and render it to the browser
 def daily_recipes_lunch_view(response):
     return render(response, "main/DailyLunch.html")
 
+#Function that will return the DailyDinner.html template and render it to the browser
 def daily_recipes_dinner_view(response):
     return render(response, "main/DailyDinner.html")
 
+#Function that will return the Holidays.html template and render it to the browser
 def holidays_view(response):
     return HttpResponse("<h1>This is the holidays page<h1>")
 
+#Function that will return the Login.html template and render it to the browser
 def login_view(response):
     return render(response, "main/Login.html")
+
+#Function that will return the Register.html template and render it to the browser
+def register_view(response):
+    if response.method == "POST":
+        form = MemberForm(response.POST or None)
+        if form.is_valid():
+            form.save()
+        return render(response, "main/Login.html")
+    else:
+        return render(response, "main/Register.html")
