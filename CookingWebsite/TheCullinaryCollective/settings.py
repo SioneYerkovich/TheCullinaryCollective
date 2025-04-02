5a18aa5d4e79d58b1dc25c30991140acea7eb6d2
Sione Yerkovich <sioneyerkovich01@gmail.com>
2025-04-02 16:13:16 +1300

    Feature (main)
    1. Imported heroku files in settings.py for deployment
    2. Created requirements.txt file with dependencies for heroku deployment
    3. Created Procfile to communicate application type to heroku

--- CookingWebsite/TheCullinaryCollective/settings.py
+++ CookingWebsite/TheCullinaryCollective/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'TheCullinaryCollective.urls'
@@ -124,6 +128,8 @@ STATICFILES_DIRS = [
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -145,3 +151,5 @@ EMAIL_HOST_USER = 'tcc.pwreset@gmail.com'
 # Your gmail app password
 EMAIL_HOST_PASSWORD = 'oggn ohis nhhc lvgq'
 #ppaognajd
+
+django_heroku.settings(locals())
